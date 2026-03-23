574a2adb9ca191e10b25f17b258ce089f35040f7
iOvermind <Overmind@hotmail.com.tw>
2026-03-23 09:19:25 +0800

    Auto update: 2026-03-23

--- build.bat
+++ build.bat
@@ -9,15 +9,16 @@ echo 🧹 正在清理舊的暫存檔...
 if exist "build" rmdir /s /q "build"
 if exist "dist" rmdir /s /q "dist"
 if exist "run_all.spec" del /q "run_all.spec"
+if exist "rich3_patch.spec" del /q "rich3_patch.spec"
 
 :: 2. 執行打包指令
 echo 🚀 開始將腳本與資料夾打包成單一 EXE...
-pyinstaller --onefile --icon=icon.png --add-data "EVENTVOC;EVENTVOC" --add-data "NEWSVOC;NEWSVOC" --add-data "SCREEN;SCREEN" run_all.py
+pyinstaller --noconsole --onefile --name rich3_patch --icon=icon.png --add-data "EVENTVOC;EVENTVOC" --add-data "NEWSVOC;NEWSVOC" --add-data "SCREEN;SCREEN" run_all.py
 
 :: 3. 檢查結果
 if %errorlevel% equ 0 (
     echo.
-    echo ✅ 打包成功！請去 dist 資料夾裡面拿你的 run_all.exe！
+    echo ✅ 打包成功！請去 dist 資料夾裡面拿你的 rich3_patch.exe！
 ) else (
     echo.
     echo ❌ 靠背，打包失敗！請往上捲看噴了什麼錯。
